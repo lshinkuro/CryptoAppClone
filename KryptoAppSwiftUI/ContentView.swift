e2db5c8fc78f02bad901dcf7b2b6e68428cd934c
nurkholis <nur.kholis@ist.id>
2022-12-04 13:11:45 +0700

    Initial Commit

---
+++ KryptoAppSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KryptoAppSwiftUI
+//
+//  Created by nur kholis on 04/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KryptoAppSwiftUI/KryptoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KryptoAppSwiftUIApp.swift
+//  KryptoAppSwiftUI
+//
+//  Created by nur kholis on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct KryptoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
